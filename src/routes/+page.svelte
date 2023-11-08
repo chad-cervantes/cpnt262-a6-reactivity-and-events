aed25b68461bc1736ab5cab225afc7ff5098dbec
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-08 15:10:58 -0700

    made the objects in grid-cols-2; added input field and decorated it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,14 @@
 </script>
 
 <!--for each loop-->
-{#each playerArr as player}
-<p class="p-4 text-center">{player.player}, {player.team}, {player.position}</p>
-{/each}
+<section class="grid grid-cols-2 grid-rows-">
+  {#each playerArr as player}
+    <p class="p-4 text-center">{player.player}, {player.team}, {player.position}</p>
+  {/each}
+</section>
+
+<!--Input field-->
+<section class="p-8 text-center">
+  <label for="add-player">Add Liverpool Player</label>
+  <input type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-4 py-1.5 pl-7 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Liverpool Player">
+</section>
