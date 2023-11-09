c267b02cf2d839fe2c8fc697ed1e2a60fc5aea25
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-08 18:18:25 -0700

    transfered the object arrays from +page.svelte to index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,16 @@
-// place files you want to import through the `$lib` alias in this folder.
+//Object Array
+  export let playerArr = [
+    {name: 'Mohamed Salah', team: 'Liverpool FC', position: 'right winger'},
+    {name: 'Virgil Van Dijk', team: 'Liverpool FC', position: 'centre back'},
+    {name: 'Alexis Mac Allister', team: 'Liverpool FC', position: 'midfielder'},
+    {name: 'Allison Becker', team: 'Liverpool FC', position: 'goalie'},
+  ];
+
+  export let newPlayerArr = {name:'', team:'', position:''};
+
+  //Add player function
+  export function addPlayer() {
+    newPlayerArr = playerArr;
+  }
+  console.log(addPlayer);
+
