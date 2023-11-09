da0c244c5749f717e3570f0e51e7ebd102f701a4
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-08 21:28:33 -0700

    put content in Event.svelte

---
+++ src/lib/components/Event.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export const dispatch = createEventDispatcher();
+  
+  export const message = () => {
+    dispatch('soccerMessage', {message: 'Added player'});
+  }
+
+</script>
+
+<!--Event Dispatch button-->
+<button on:click={message}>Add Player</button>
