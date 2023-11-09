1d22a24bae2963bec092fd87d97fc1766000d955
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-09 15:07:31 -0700

    changed background color of Add Player button from blue to orange

--- src/lib/components/EventDispatcher.svelte
+++ src/lib/components/EventDispatcher.svelte
@@ -11,4 +11,4 @@
 
 
 <!--Button to add a new player-->
-<button on:click={defaultFn} class="flex m-auto rounded-md bg-blue-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-blue-700 active:bg-blue-900">Add Player</button>
+<button on:click={defaultFn} class="flex m-auto rounded-md bg-orange-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-orange-700 active:bg-orange-900">Add Player</button>
