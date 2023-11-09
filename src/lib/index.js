ca407e8051d7bfd558479c6f74b37c6472fc8fa2
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-09 11:13:19 -0700

    transferred object array to +page.svelte

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,3 @@
-//Object Array
-  export let playerArr = [
-    {name: 'Mohamed Salah', team: 'Liverpool FC', position: 'right winger'},
-    {name: 'Virgil Van Dijk', team: 'Liverpool FC', position: 'centre back'},
-    {name: 'Alexis Mac Allister', team: 'Liverpool FC', position: 'midfielder'},
-    {name: 'Allison Becker', team: 'Liverpool FC', position: 'goalie'},
-  ];
 
-  export let newPlayer = '';
-  export let newPlayerName = '';
-  export let newPlayerTeam = '';
-  export let newPlayerPosition = '';
+
 
