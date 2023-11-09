184e960748bd4ade60a61024c848f8dcbcca45e0
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-09 11:15:03 -0700

    transferred Event prop; made function to update values, but isn't working so far

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,63 @@
 <script>
-  //Arrays from index.js
-  import {playerArr} from '$lib/index.js';
-  import {newPlayerName} from '$lib/index.js';
-  import {newPlayerTeam} from '$lib/index.js';
-  import {newPlayerPosition} from '$lib/index.js';
- 
-  import {newPlayer} from '$lib/index.js';
+ //Object Array
+  let playerArr = [
+    {name: 'Mohamed Salah', team: 'Liverpool FC', position: 'right winger'},
+    {name: 'Andy Robertson', team: 'Liverpool FC', position: 'left back'},
+    {name: 'Allison', team: 'Liverpool FC', position: 'goalie'},
+  ];
 
-  import Event from '$lib/components/Event.svelte';
+  console.log(playerArr);
+  
+  import Event from '../lib/components/Event.svelte';
+  
+  let newName = {name: ''};
+  let newTeam = {team: ''};
+  let newPosition = {position: ''};
+  
+  function update() {
+    // playerArr.name = '';
+    // playerArr.team = '',
+    // playerArr.position = '';
+    newName.name = '';
+    newTeam.team = '';
+    newPosition.position = '';
 
-  //Add player function
- function addPlayer() {
-    const newPlayer = {
-      name: newPlayerName,
-      team: newPlayerTeam,
-      position: newPlayerPosition,
-    }
-  };
+  }
+
+
 </script>
 
-<!--for each loop-->
-<section class="grid grid-cols-2">
-  {#each playerArr as player}
-  <ul class="p-4">
-    <li class="text-center">{player.name}</li>
-    <li class="text-center">{player.team}</li>
-    <li class="text-center">{player.position}</li>
-  </ul>
+<!--for each loop for playerArr-->
+<!-- <section class="grid grid-cols-2"> -->
+  {#each playerArr as {name, team, position}}
+    <p>{name} is a soccer player from {team}, and his position is {position}</p>
   {/each}
-</section>
+<!-- </section> -->
+
 
 <!--Input field-->
 <!--Add Name-->
 <section class="p-4 text-center">
   <label for="add-player">Add Name</label>
-  <input type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Name">
+  <input bind:value={newName} type="text" id="add-player"  class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Name">
 </section>
 
 <!--Add Team-->
 <section class="p-4 text-center">
   <label for="add-player">Add Team</label>
-  <input type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Team">
+  <input bind:value={newTeam} type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Team">
 </section>
 
-<!--Add Position-->
 <section class="p-4 text-center">
   <label for="add-player">Add Position</label>
-  <input type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Position">
+  <input bind:value={newPosition} type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Position">
 </section>
 
-<!--Button to add a new player-->
-<button on:click={addPlayer} class="m-auto rounded-md bg-blue-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-blue-700 active:bg-blue-900">Click To Add Player</button>
+<!--if/else state from <Event />-->
 
-<Event />
+
+
+<Event on:button={update}/>
+
+
 
