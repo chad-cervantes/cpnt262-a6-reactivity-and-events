0e14ced276894d2d902aa73412e6c7042ebd1b64
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-08 18:19:14 -0700

    imported object arrays from index.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,39 @@
 <script>
-  //Object Array
-  let playerArr = [
-    {player: 'Mohamed Salah', team: 'Liverpool FC', position: 'right winger'},
-    {player: 'Virgil Van Dijk', team: 'Liverpool FC', position: 'centre back'},
-    {player: 'Alexis Mac Allister', team: 'Liverpool FC', position: 'midfielder'},
-    {player: 'Allison Becker', team: 'Liverpool FC', position: 'goalie'},
-  ];
+  //Arrays from index.js
+  import {playerArr} from '$lib/index.js';
+  import {newPlayerArr} from '$lib/index.js';
+  import {addPlayer} from '$lib/index.js';
 </script>
 
 <!--for each loop-->
-<section class="grid grid-cols-2 grid-rows-">
+<section class="grid grid-cols-2">
   {#each playerArr as player}
-    <p class="p-4 text-center">{player.player}, {player.team}, {player.position}</p>
+  <ul class="p-4">
+    <li class="text-center">{player.name}</li>
+    <li class="text-center">{player.team}</li>
+    <li class="text-center">{player.position}</li>
+  </ul>
   {/each}
 </section>
 
 <!--Input field-->
-<section class="p-8 text-center">
-  <label for="add-player">Add Liverpool Player</label>
-  <input type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-4 py-1.5 pl-7 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Liverpool Player">
+<!--Add Name-->
+<section class="p-4 text-center">
+  <label for="add-player">Add Name</label>
+  <input bind:value={newPlayerArr.name} type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Name">
 </section>
+
+<!--Add Team-->
+<section class="p-4 text-center">
+  <label for="add-player">Add Team</label>
+  <input bind:value={newPlayerArr.team} type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Team">
+</section>
+
+<!--Add Position-->
+<section class="p-4 text-center">
+  <label for="add-player">Add Position</label>
+  <input bind:value={newPlayerArr.position} type="text" id="add-player" class="block m-auto w-[150px] rounded-lg border-4 border-black p-2 placeholder:text-slate-400  sm:text-sm sm:leading-6" placeholder="Position">
+</section>
+
+<!--Button to add a new player-->
+<button on:click={addPlayer} class="m-auto rounded-md bg-blue-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-blue-700 active:bg-blue-900">Click To Add Player</button>
