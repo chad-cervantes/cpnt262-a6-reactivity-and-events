43c8a673cc021868b61a94f1290a72d943a8dbf9
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-09 14:47:58 -0700

    made function for dark mode button; made actual button; styled button when it turned to dark mode

---
+++ src/lib/components/DarkMode.svelte
@@ -0,0 +1,18 @@
+<script>
+  //Function to turn the page dark
+  function darkModeButton () {
+    window.document.body.classList.toggle('dark-side')
+  }
+</script>
+
+<!--button for dark mode toggle-->
+<button on:click={darkModeButton} class="bg-rose-300 text-white rounded-md px-8 py-4 mx-5 transition ease-linear duration-150">
+  Click for light/dark mode <slot />
+</button>
+
+<style>
+  :global(body.dark-side) button {
+    background-color: darkgoldenrod;
+    color: white;
+  }
+</style>
