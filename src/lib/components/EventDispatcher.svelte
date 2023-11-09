2dbc8462a266c9ac960d13e1e4afb3bd6f03b4ef
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-09 11:54:53 -0700

    changed Event.svelte to EventDispatcher.svelte

---
+++ src/lib/components/EventDispatcher.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  
+  function defaultFn() {
+      dispatch('button', {});
+  }
+
+</script>
+
+
+<!--Button to add a new player-->
+<button on:click={defaultFn} class="m-auto rounded-md bg-blue-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-blue-700 active:bg-blue-900">Click To Add Values</button>
