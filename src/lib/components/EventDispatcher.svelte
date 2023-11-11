d98669f6c4e02fe65d6cb099cc2861aca3926191
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-11 10:22:29 -0700

    centered the button

--- src/lib/components/EventDispatcher.svelte
+++ src/lib/components/EventDispatcher.svelte
@@ -11,4 +11,6 @@
 
 
 <!--Button to add a new player-->
-<button on:click={defaultFn} class="flex m-auto rounded-md bg-orange-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-orange-700 active:bg-orange-900">Add Player</button>
+<div class="flex justify-center p-4">
+  <button on:click={defaultFn} class="flex justify-center rounded-md bg-orange-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-orange-700 active:bg-orange-900">Add Player</button>
+</div>
