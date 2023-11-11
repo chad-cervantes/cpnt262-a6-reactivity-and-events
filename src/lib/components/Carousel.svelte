e597c4b41423649d5b4735085003bdfd989ca12a
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-11 10:20:54 -0700

    made the button smaller in size & centered it

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -40,8 +40,8 @@ function prevSlide() {
 
 <!--Making of carousel slide-->
 <section>
-  <h2>{currentSlideIndex}: {currentSlideContent}</h2>
-  <div>
+  <h2 class="text-center">{currentSlideIndex}: {currentSlideContent}</h2>
+  <div class="flex justify-center p-4 gap-4">
     <button on:click={prevSlide} class="rounded-md bg-green-300 px-4 py-2" id="prev-slide">Previous</button>
     <button on:click={nextSlide} class="rounded-md bg-green-300 px-4 py-2" id="next-slide">Next</button>
   </div>
