b48c7de630ce1abae2ddaf774c8cc0212174f04a
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-12 17:57:57 -0700

    changed event modifier to preventDefault

--- src/lib/components/EventDispatcher.svelte
+++ src/lib/components/EventDispatcher.svelte
@@ -12,5 +12,5 @@
 
 <!--Button to add a new player-->
 <div class="flex justify-center p-4">
-  <button on:click|once={defaultFn} class="flex justify-center rounded-md bg-orange-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-orange-700 active:bg-orange-900">Add Player</button>
+  <button on:click|preventDefault={defaultFn} class="flex justify-center rounded-md bg-orange-500 px-4 py-2 mx-4 text-white transition ease-linear duration-150 hover:bg-orange-700 active:bg-orange-900">Add Player</button>
 </div>
